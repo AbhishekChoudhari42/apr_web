7c10b9a74c9df571ca00b66ef7c6ca3098fe13f9
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2024-02-11 16:26:34 +0530

    hero section added

---
+++ app/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='h-[90vh] w-full text-white flex justify-center items-center flex-col '>
+        <h1 className='white-gradient font-bold text-center text-5xl leading-[3.5rem]'>Transform your Ideas <br/> 
+        into Digital Masterpieces</h1>
+        <p className='font-semibold text-xl text-neutral-500'>Crafting Bespoke Websites that Drive Success</p>
+    </div>
+  )
+}
+
+export default Hero
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='absolute top-0 left-0 px-4 w-full flex justify-between items-center py-2'>
+        <h1 className='text-white font-bold text-lg'>
+          Apricity 
+          <span className='text-teal-500 mx-[2px] text-xl'>.</span> 
+          tech
+        </h1>
+        <div className='gradient-container-grey'>
+
+        <div className='nav-btns '>
+          <button className='nav-btn-element'>
+            <p className='nav-btn-text'>Home</p>
+          </button>
+          <button className='nav-btn-element'>
+            <p className='nav-btn-text'>Services</p>
+          </button>
+          <button className='nav-btn-element'>
+            <p className='nav-btn-text'>Portfolio</p>
+          </button>
+          <button className='nav-btn-element'>
+            <p className='nav-btn-text'>Contact</p>
+          </button>
+        </div>
+        </div>
+
+        
+        <button className='cta text-xs border w-36 h-[38px]  border-neutral-950 px-[4px] text-white rounded-full'>
+          <p className='nav-btn-text ml-2'>Become a client</p>
+          <div className='rounded-full w-[32px] h-[32px] border '>
+
+          </div>
+        </button>
+        
+    </div>
+  )
+}
+
+export default Navbar
