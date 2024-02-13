9f1b1d4bc0bb0036c1a32e8f49c4b6cdd55c9ece
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2024-02-13 22:19:32 +0530

    card style changes

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
     return (
         <div className=' h-[100vh] text-white flex flex-col gap-14 items-center relative border-t border-neutral-900 rounded-tr-[40px] rounded-tl-[40px] pt-12 z-[0] bg-black'>
             <h1 className='white-gradient-text page-title ' data-text="SERVICES">SERVICES</h1>
-            <div className="card-container bg-cross-lines ">
+            <div className="card-container">
                 <div className="card ">
                     <h2>Web Design</h2>
                     <p>From stylish, contemporary designs to striking layouts,
@@ -16,11 +16,11 @@ const Services = () => {
                     <Image width={100} height={100} alt='icon' src={"/temp.png"}/>
                 </div>
             </div>
-            <div className="card-container bg-cross-lines ">
+            <div className="card-container ">
                 <div className="card ">
                     <h2>Web Development</h2>
-                    <p>Our skilled developers transform your design into reality  
-                    <br />with pristine code and flawless functionality,  
+                    <p>Our skilled developers transform your design into reality 
+                    <br /> with pristine code and flawless functionality,  
                     <br />guaranteeing seamless website performance.</p>
                 </div>
                 <div className="absolute right-0 top-[50%] translate-y-[-50%] translate-x-[50%]">
