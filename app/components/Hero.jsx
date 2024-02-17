c30105a61be8e023cd4188b770987b00ddfccfde
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2024-02-17 10:11:46 +0530

    card animation update

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className='h-[90vh] w-full text-white flex justify-center items-center flex-col '>
         <h1 className='white-gradient-text font-bold text-center text-5xl leading-[3.5rem]'>Transform your Ideas <br/> 
         into Digital Masterpieces</h1>
-        <p className='font-semibold text-xl white-gradient-text opacity-50 -mt-6'>Crafting Bespoke Websites that Drive Success</p>
+        <p className='font-semibold text-xl white-gradient-text opacity-50 -mt-6'>Crafting Websites that Drive Success</p>
     </div>
   )
 }
